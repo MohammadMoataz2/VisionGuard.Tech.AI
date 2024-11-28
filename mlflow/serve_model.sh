6e75311b70877c617100eaee7f309f04d24f3cb6
mohammad obeid <mohammado@sanadcash.com>
2024-11-29 01:47:02 +0300

    plan b start

--- mlflow/serve_model.sh
+++ mlflow/serve_model.sh
@@ -5,6 +5,10 @@ export MLFLOW_TRACKING_URI=http://localhost:5000
 mlflow ui &
 
 # Serve the FaceDetectionModel
-mlflow models serve -m "models:/FaceDetectionModel/1" --port 5004 -h 0.0.0.0
+mlflow models serve -m "models:/FaceDetectionModel/1" --port 5003 &
 
+# Serve the DeepFaceAnalyzeModel
+mlflow models serve -m "models:/DeepFaceAnalyzeModel/1" --port 5004 &
+
+# Wait for all background processes to finish
 wait
